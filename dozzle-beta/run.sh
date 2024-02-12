826bbbcceec661a498f7a23494008563184eb4c4
Stefano C <caltaste@inwind.it>
2024-02-12 14:30:56 +0100

    Update run.sh

--- dozzle-beta/run.sh
+++ dozzle-beta/run.sh
@@ -1,2 +1,5 @@
 #/bin/sh
-docker run --detach --volume=/var/run/docker.sock:/var/run/docker.sock -p 8080:8080 amir20/dozzle 
+
+#dozzle
+
+#docker run --detach --volume=/var/run/docker.sock:/var/run/docker.sock -p 8080:8080 amir20/dozzle 
