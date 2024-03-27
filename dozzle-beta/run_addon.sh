96153efcea3695146fa63115ac565c2e275d5a7f
Stefano C <caltaste@inwind.it>
2024-03-27 13:54:51 +0100

    Delete dozzle-beta directory

--- dozzle-beta/run_addon.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-#eval $(jq --raw-output '.env_var | .[] | "export " + .name + "=\"" + .value + "\""' /data/options.json)
-
-#chown -R grafana:grafana /data
-docker run -d --name=dozzle -p 8080:8080 --privileged -v /var/run/docker.sock:/var/run/docker.sock amir20/dozzle:latest
-
-#go dev /run.sh
