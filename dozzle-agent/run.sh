ba2e8820834c0bf9a03064c3553305c60b23c043
Stefano C. <caltaste@inwind.it>
2025-12-27 00:51:06 +0100

    Update download URLs to specific version v8.14.12

--- dozzle-agent/run.sh
+++ dozzle-agent/run.sh
@@ -8,13 +8,13 @@ echo "Arch rilevata: $ARCH"
 # Seleziona binario corretto
 case "$ARCH" in
   x86_64)
-    URL="https://github.com/amir20/dozzle/releases/latest/download/dozzle_linux_amd64.tar.gz"
+    URL="https://github.com/amir20/dozzle/archive/refs/tags/v8.14.12.tar.gz"
     ;;
   aarch64)
-    URL="https://github.com/amir20/dozzle/releases/latest/download/dozzle_linux_arm64.tar.gz"
+    URL="https://github.com/amir20/dozzle/archive/refs/tags/v8.14.12.tar.gz"
     ;;
   armv7* | armv7l)
-    URL="https://github.com/amir20/dozzle/releases/latest/download/dozzle_linux_armv7.tar.gz"
+    URL="https://github.com/amir20/dozzle/archive/refs/tags/v8.14.12.tar.gz"
     ;;
   *)
     echo "Arch non supportata: $ARCH"
