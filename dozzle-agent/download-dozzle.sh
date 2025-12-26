627a3042af0b638c407fc692bb561f17a62bd6eb
Stefano C. <caltaste@inwind.it>
2025-12-27 00:27:32 +0100

    Refactor download script to use case statement

--- dozzle-agent/download-dozzle.sh
+++ dozzle-agent/download-dozzle.sh
@@ -2,17 +2,23 @@
 set -e
 
 ARCH=$(uname -m)
+echo "Arch rilevata: $ARCH"
 
-if [ "$ARCH" = "x86_64" ]; then
+case "$ARCH" in
+  x86_64)
     URL="https://github.com/amir20/dozzle/releases/latest/download/dozzle_linux_amd64.tar.gz"
-elif [ "$ARCH" = "aarch64" ]; then
+    ;;
+  aarch64)
     URL="https://github.com/amir20/dozzle/releases/latest/download/dozzle_linux_arm64.tar.gz"
-elif [ "$ARCH" = "armv7l" ]; then
+    ;;
+  armv7* | armv7l)
     URL="https://github.com/amir20/dozzle/releases/latest/download/dozzle_linux_armv7.tar.gz"
-else
+    ;;
+  *)
     echo "Arch non supportata: $ARCH"
     exit 1
-fi
+    ;;
+esac
 
 echo "Scarico Dozzle da $URL"
-curl -L $URL | tar -xz -C /usr/local/bin
+curl -fsSL "$URL" | tar -xz -C /usr/local/bin
